607f92e59cedde1759430f84e71573e157c3dd47
Dmitriy <Dmitriy@DESKTOP-8IVVVVB>
2022-11-01 22:22:01 +0300

    Добавьте файлы проекта.

---
+++ App1/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    internal class BankAccount<T> // Счет
+    {
+        private T Number { get; set; }
+        private double Balance { get; set; }
+        private string Name { get; set; }
+        public string GetInfo() // Вывод информации о счете
+        {
+            return $"\nНомер счета: {Number} \nБаланс: {Balance} \nФИО: {Name}\n";
+        }
+
+        public void Input() // Ввод информации о счете
+        {
+            Console.WriteLine("Введите номер счета");
+            string stringNumber = Console.ReadLine();
+            Number = (T)Convert.ChangeType(stringNumber, typeof(T));
+
+            Console.WriteLine("Введите баланс");
+            Balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите имя");
+            Name = Console.ReadLine();
+
+        }
+    }
+}
---
+++ App1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount<int> bankAccount1 = new BankAccount<int>();
+            bankAccount1.Input();
+            Console.WriteLine(bankAccount1.GetInfo());
+
+            BankAccount<string> bankAccount2 = new BankAccount<string>();
+            bankAccount2.Input();
+            Console.WriteLine(bankAccount2.GetInfo());
+
+            BankAccount<double> bankAccount3 = new BankAccount<double>();
+            bankAccount3.Input();
+            Console.WriteLine(bankAccount3.GetInfo());
+
+            Console.ReadKey();
+
+        }
+    }
+}
